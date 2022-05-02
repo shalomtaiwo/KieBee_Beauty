c246d36f09c76eafac79368e2cd6ff1c8b18e545
Shalom Taiwo <ShalomTaiwo@my.uopeople.edu>
2022-05-02 15:26:42 +0200

    about/shop coming soon alert

--- kiebee.js
+++ kiebee.js
@@ -8,16 +8,24 @@ class kieBee{
     navPage(){
         this.contactUs = document.querySelector('#contactUs');
         this.contactPage = document.querySelector('#contactPage');
+        this.aboutLink = document.querySelector('#aboutUs');
+        this.shopLink = document.querySelector('#shopWithUs');
         this.homeLink = document.querySelector('#homePage');
         this.homePage = document.querySelector('.homePage');
         this.contactUs.addEventListener('click',(event)=>{
             this.contactPage.style.display = "flex";
             this.homePage.style.display = "none";
-        })
+        });
         this.homeLink.addEventListener('click',(event)=>{
             this.contactPage.style.display = "none";
             this.homePage.style.display = "block";
-        })
+        });
+        this.aboutLink.addEventListener('click',(event)=>{
+            alert('Coming Soon! Feel free to send us a message and subscribe to stay in tuned!');
+        });
+        this.shopLink.addEventListener('click',(event)=>{
+            alert('Coming Soon! Feel free to send us a message and subscribe to stay in tuned!');
+        });
     }
 
     subscribePopup(){
@@ -29,14 +37,14 @@ class kieBee{
             }, 2000);
         this.closeSubs.addEventListener('click',(event)=>{
             this.subsColumn.style.display = "none";
-        })
+        });
     }
 
     formSubmitEvents(){
         this.form = document.querySelector('form');
         document.addEventListener('submit',(event)=>{
             event.preventDefault();
-        })
+        });
     }
 }
 
